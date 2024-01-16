ccf833fb8397ddbfbf6d9fcfdfa3ba43848ab1d7
Mohammad Ali Gholizadeh <moal.gholi@gmail.com>
2024-01-16 17:25:10 +0330

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,130 @@
+<script setup>
+const getLoc = async () => {
+  const url =
+    "http://ip-api.com/json/?fields=status,country,city,lat,lon,timezone";
+
+  const response = await fetch(url);
+  const data = response.json();
+
+  return data;
+};
+const getWeather = async (lat, lon) => {
+  api = "f0894defae7c5584798f8812232a40c2";
+
+  url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`;
+
+  const response = await fetch(url);
+  const data = response.json();
+
+  return data;
+};
+function getIcon(weMain) {
+  let icon;
+  switch (weMain) {
+    case "Thunderstorm":
+      icon = `${weMain}.svg`;
+      break;
+    case "Drizzle":
+      icon = `${weMain}.svg`;
+      break;
+    case "Rain":
+      icon = `${weMain}.svg`;
+      break;
+    case "Snow":
+      icon = `${weMain}.svg`;
+      break;
+    case "Clear":
+      const DayOrNigh = getDayOrNight();
+      icon = `${weMain}-${DayOrNigh}.svg`;
+      break;
+    case "Clouds":
+      icon = `${weMain}.svg`;
+      break;
+    case "Atmosphere":
+      icon = `${weMain}.png`;
+      break;
+  }
+  return icon;
+}
+function getDayOrNight() {
+  let DayOrNigh;
+  var d = new Date();
+
+  const hour = d.getHours();
+
+  if (hour >= 6 && hour <= 19) {
+    DayOrNigh = "Day";
+  } else {
+    DayOrNigh = "Night";
+  }
+
+  return DayOrNigh;
+}
+function getTemp(weTemp) {
+  const k = weTemp;
+  const f = ((k - 273.15) * 9) / 5 + 32;
+  const c = k - 273.15;
+  return (temp = {
+    kel: Math.floor(k),
+    far: Math.floor(f),
+    can: Math.floor(c),
+  });
+}
+getLoc().then((locData) => {
+  const timeZone = locData.timezone;
+  console.log(locData);
+  getWeather(locData.lat, locData.lon).then(weData);
+});
+</script>
+
 <template>
-  <p>ایندکس</p>
+  <h1 id="opencode">نمونه اپ آب و هوایی</h1>
+  <div class="location">
+    <h1 class="timezone">{{ $timeZone }}</h1>
+    <p class="icon">icon</p>
+  </div>
+  <div class="temperature">
+    <div class="degree-section">
+      <h2 class="degree">20</h2>
+      <span>C</span>
+    </div>
+    <div class="temperature-description">temperature-description</div>
+  </div>
 </template>
+
+<style>
+#opencode {
+  font-size: 45px;
+  margin-bottom: 100px;
+}
+.icon img {
+  height: 256px;
+  width: 256px;
+}
+.location,
+.temperature {
+  height: 30vh;
+  /* width: 50%; */
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.temperature {
+  flex-direction: column;
+}
+.temperature-description {
+  font-size: 25px;
+}
+.degree-section {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+.degree-section span {
+  margin: 10px;
+  font-size: 30px;
+}
+.degree-section h2 {
+  font-size: 40px;
+}
+</style>
